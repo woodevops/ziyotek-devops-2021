7030f134d795ff63aaf55e695a4cc596c6ec988a
rpaskalev <rpaskalev@lomixtech.com>
2022-01-19 21:33:44 -0500

    adding alb and asg

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,12 @@
-  #!/bin/bash
-  # sudo yum update -y
-  # sudo yum install -y httpd
-  # sudo systemctl start httpd.service
-  # sudo systemctl enable httpd.service
-  # sudo echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
+  !/bin/bash
+  sudo yum update -y
+  sudo yum install -y httpd
+  sudo systemctl start httpd.service
+  sudo systemctl enable httpd.service
+  sudo echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
 
-sudo amazon-linux-extras install postgresql10
+# sudo amazon-linux-extras install postgresql10
