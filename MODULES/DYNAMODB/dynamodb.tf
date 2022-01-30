a6370288e0bb33dc2327c75020b051b4061eb6c8
rpaskalev <rpaskalev@lomixtech.com>
2022-01-29 21:40:39 -0500

    convert iam and dynamodb to modules

---
+++ MODULES/DYNAMODB/dynamodb.tf
@@ -0,0 +1,11 @@
+resource "aws_dynamodb_table" "ziyotek_devops_dynamodb_lock" {
+  name     = "terraform-lock"
+  hash_key = var.dynamodb_hash_key
+  read_capacity = var.dynamodb_read_capacity
+  write_capacity = var.dynamodb_write_capacity
+
+  attribute {
+    name = "LockID"
+    type = var.dynamodb_attribute_type
+  }
+} 
---
+++ SBX1/iam_instance_profile.tf
@@ -0,0 +1,5 @@
+module "iam_ip" {
+    source = "../MODULES/IAM"
+    environment = var.environment
+    s3_arn = module.s3.s3_arn
+}
